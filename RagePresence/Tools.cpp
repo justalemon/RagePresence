99daabe1099e46c796bacb72702f2dbbc8627cd1
Hannele Ruiz <justlemoncl@gmail.com>
2021-03-06 03:46:44 -0300

    Added tool to show a notification

---
+++ RagePresence/Tools.cpp
@@ -0,0 +1,8 @@
+#include <natives.hpp>
+
+void ShowNotification(const char* message)
+{
+	HUD::BEGIN_TEXT_COMMAND_THEFEED_POST("CELL_EMAIL_BCON");
+	HUD::ADD_TEXT_COMPONENT_SUBSTRING_PLAYER_NAME(message);
+	HUD::END_TEXT_COMMAND_THEFEED_POST_TICKER(false, true);
+}
