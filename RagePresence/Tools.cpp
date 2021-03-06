a7563f9cf9ff635fdca37189cc10a8419afc316a
Hannele Ruiz <justlemoncl@gmail.com>
2021-03-06 19:06:38 -0300

    Fixed order of the tools

--- RagePresence/Tools.cpp
+++ RagePresence/Tools.cpp
@@ -1,15 +1,15 @@
 #include <natives.hpp>
 
-void ShowDebugMessage(const char* message)
-{
-	#if DEBUG
-	ShowNotification(message);
-	#endif
-}
-
 void ShowNotification(const char* message)
 {
 	HUD::BEGIN_TEXT_COMMAND_THEFEED_POST("CELL_EMAIL_BCON");
 	HUD::ADD_TEXT_COMPONENT_SUBSTRING_PLAYER_NAME(message);
 	HUD::END_TEXT_COMMAND_THEFEED_POST_TICKER(false, true);
+}
+
+void ShowDebugMessage(const char* message)
+{
+	#if DEBUG
+	ShowNotification(message);
+	#endif
 }
